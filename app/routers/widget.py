a22c41dea1262fe7fa2ac3f63662331a8ea1e3a3
KirillAISREDA <humancoins@gmail.com>
2026-04-10 21:06:08 +0300

    Lower desktop chat btn by 5px

--- app/routers/widget.py
+++ app/routers/widget.py
@@ -117,7 +117,7 @@ async def widget_embed_script(request: Request):
       '<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>';
     btn.style.cssText = [
       'position:fixed',
-      'bottom:' + cfg.offsetY + 'px',
+      'bottom:' + (cfg.offsetY - 5) + 'px',
       side + ':' + cfg.offsetX + 'px',
       oppositeSide + ':auto',
       'width:60px',
